be248508becc89fc95037a6828c51d99d602eea5
matheusps <matheus.ps@icloud.com>
2019-05-03 18:03:00 -0300

    Using tachyons

--- react/next/Styled.tsx
+++ react/next/Styled.tsx
@@ -7,84 +7,63 @@ interface SliderTrackProps {
 }
 
 interface SlideProps {
-  flex: string
-  width: string
-  position: string
+  shouldRenderOnSSR?: boolean
+  domFullyLoaded?: boolean
+  basis?: string | number
+  width?: string | number
 }
 
 interface DotProps {
   isActive: boolean
 }
 
-export const StyledArrow = styled.button`
+export const StyledArrow = styled('button')`
   position: absolute;
-  outline: none;
-  transition: all 0.5s;
-  z-index: 1000;
   border: none;
-  background: rgba(0, 0, 0, 0.2);
-  min-width: 3rem;
-  min-height: 3rem;
+  outline: none;
+  background: transparent;
   cursor: pointer;
-
-  :hover {
-    background: rgba(0, 0, 0, 0.8);
-  }
-
-  ::before {
-    color: #fff;
+  :before {
+    content: '';
     display: block;
-    text-align: center;
-    z-index: 2;
     position: relative;
+    text-align: center;
+    width: 2rem;
+    height: 2rem;
+    border-top: solid 0.15rem currentColor;
+    border-right: solid 0.15rem currentColor;
   }
 `
 
 export const StyledLeftArrow = styled(StyledArrow)`
   left: calc(3% + 1px);
   :before {
-    content: '⏪';
+    transform: rotate(225deg);
   }
 `
 
 export const StyledRightArrow = styled(StyledArrow)`
   right: calc(3% + 1px);
   :before {
-    content: '⏩';
+    transform: rotate(45deg);
   }
 `
 
-export const SliderList = styled.div`
-  display: flex;
-  align-items: center;
-  overflow: hidden;
-  position: relative;
-`
+export const SliderTrack = styled('div')<SliderTrackProps>(
+  {
+    willChange: 'transform',
+  },
+  props => ({
+    transition: props.transition,
+    overflow: props.shouldRenderOnSSR ? 'hidden' : 'unset',
+    transform: `translate3d(${props.transform}px,0,0)`,
+  })
+)
 
-export const SliderTrack = styled.ul<SliderTrackProps>`
-  list-style: none;
-  padding: 0;
-  margin: 0;
-  display: flex;
-  flex-direction: row;
-  position: relative;
-  will-change: transform;
-  transition: ${props => props.transition};
-  overflow: ${props => (props.shouldRenderOnSSR ? 'hidden' : 'unset')};
-  transform: ${props => `translate3d(${props.transform}px,0,0)`};
-  @media all and (-ms-high-contrast: none), (-ms-high-contrast: active) {
-    overflow: visible !important;
-  }
-`
-
-export const StyledSlide = styled.li<SlideProps>`
-  flex: ${props => props.flex};
-  position: ${props => props.position};
-  width: ${props => props.width};
-  @media all and (-ms-high-contrast: none), (-ms-high-contrast: active) {
-    flex-shrink: 0 !important;
-  }
-`
+export const StyledSlide = styled('div')<SlideProps>(props => ({
+  flex: props.shouldRenderOnSSR ? `1 0 ${props.basis}%` : 'auto',
+  width: props.domFullyLoaded ? `${props.width}px` : 'auto',
+}))
 
 export const StyledDotList = styled.ul`
   position: absolute;
