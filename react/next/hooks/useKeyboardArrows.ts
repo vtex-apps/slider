3bfe54bba3682d26718174a19488819ac37e830b
matheusps <cout.matheusps@gmail.com>
2019-05-24 17:31:10 -0300

    Comments

--- react/next/hooks/useKeyboardArrows.ts
+++ react/next/hooks/useKeyboardArrows.ts
@@ -1,6 +1,11 @@
 import { useEffect } from 'react'
 
-//state.domLoaded, state.currentSlide
+/**
+ * Adds event listener to call passed functions by pressing the left or right arrows
+ * @param onLeft Function that is called on left arrow key press
+ * @param onRight Function that is called on arrow arrow key press
+ * @param deps Dependencies
+ */
 const useKeyboardArrows = (
   onLeft: () => void,
   onRight: () => void,
